f57714cdafaedd3f679de388fa8cf00723c4e408
prashantjohny <prasp3677@gmail.com>
2024-12-04 15:33:42 +0530

    react-hooks

---
+++ Day11/react-hooks-app/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import CounterApp from "./components/CounterApp.jsx";
+import ImageApp from "./components/ImageApp.jsx";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import MainLayout from "./components/MainLayout.jsx";
+const App = () => {
+    return(
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainLayout/>}>
+                    <Route path={"/counterapp"} element={<CounterApp/>}/>
+                    <Route path={"/imageapp"} element={<ImageApp/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
+export default App;
